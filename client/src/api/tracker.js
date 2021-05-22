95b5553e889e8c72f35c79958e5436f1158ca7d3
Illyas <illyas.deulect@gmail.com>
2021-05-22 17:58:43 +0530

    Automatic authentication

--- client/src/api/tracker.js
+++ client/src/api/tracker.js
@@ -1,5 +1,27 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-community/async-storage';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: 'https://track-my-path.herokuapp.com/',
 });
+
+// below code added to send token automatically to request if there is token available
+// by default there is a confi we sending while making axios request above.
+// by writing below code we updating that config and pass header through that.
+//2 functions as argument in below intercepter.
+// first one called automatically any time made a request
+// second one called if there is an error
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    // TODO: verify whether this error gets returned to original place we calling api
+    return Promise.reject(err);
+  },
+);
